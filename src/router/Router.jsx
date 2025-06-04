a2b03580ac6df76354f83fbf889e19b849473958
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-06-04 21:56:11 +0600

    basic setup and packages downloaded

---
+++ src/router/Router.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { createBrowserRouter } from "react-router";
+
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
+
+export default Router;
