3547981b94279bea55cda295796c6362ac4f2613
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-06-05 21:39:30 +0600

    updated router, navbar and added login, register

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,17 +2,27 @@ import React from "react";
 import { createBrowserRouter } from "react-router";
 import HomeLayout from "../layout/HomeLayout";
 import Home from "../components/Home/Home";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
 
 const Router = createBrowserRouter([
   {
     path: "/",
     element: <HomeLayout></HomeLayout>,
-    children:[
-        {
-            path: "",
-            element: <Home></Home>,
-        }
-    ]
+    children: [
+      {
+        path: "",
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>
+      },
+      {
+        path: "/register",
+        element: <Register></Register>
+      }
+    ],
   },
 ]);
 
