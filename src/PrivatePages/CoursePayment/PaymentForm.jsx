93215fcec09269aca6d5f66aa516e3e60abc820a
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-07-31 12:50:54 +0600

    updated payment form and course details page

--- src/PrivatePages/CoursePayment/PaymentForm.jsx
+++ src/PrivatePages/CoursePayment/PaymentForm.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import UseAuth from '../../Hooks/UseAuth';
-import UseAxiosSecure from '../../Hooks/UseAxiosSecure';
-import { useElements, useStripe } from '@stripe/react-stripe-js';
-import { useLoaderData, useNavigate } from 'react-router';
+import React, { useEffect, useState } from "react";
+import UseAuth from "../../Hooks/UseAuth";
+import UseAxiosSecure from "../../Hooks/UseAxiosSecure";
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useLoaderData, useNavigate } from "react-router";
+import Swal from "sweetalert2";
+import paymentLottie from "../../assets/lotties/Online Payment.json";
 
 const PaymentForm = () => {
   const { user } = UseAuth();
@@ -11,13 +13,84 @@ const PaymentForm = () => {
   const elements = useElements();
   const navigate = useNavigate();
 
+  const [clientSecret, setClientSecret] = useState("");
+  const [loading, setLoading] = useState(false);
+
   const courseData = useLoaderData();
-  
-    return (
-        <div>
-            
-        </div>
-    );
+  const { courseId } = location.state || {};
+
+  const course = courseData.find((c) => c._id.toString() === courseId);
+  const coursePrice = course.price;
+
+    useEffect(() => {
+        axiosSecure
+        .post("/create-payment-intent", {
+            email: user.email,
+            amount: coursePrice,
+        })
+        .then((res) => setClientSecret(res.data.clientSecret));
+    }, [axiosSecure, user?.email, coursePrice]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!stripe || !elements || !clientSecret) return;
+
+        setLoading(true);
+
+        const card = elements.getElement(CardElement);
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+        type: "card",
+        card,
+        });
+
+        if (error) {
+        Swal.fire("Error", error.message, "error");
+        setLoading(false);
+        return;
+        }
+
+    const { paymentIntent, error: confirmError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: paymentMethod.id,
+      });
+
+    if (confirmError) {
+      Swal.fire("Error", confirmError.message, "error");
+      setLoading(false);
+    } else if (paymentIntent.status === "succeeded") {
+      await axiosSecure.patch(`/usersCourses/paid/${user.email}`, {
+        paid: true,
+      });
+      Swal.fire("Success", "Course payment successful", "success").then(() => {
+        navigate("/");
+      });
+    }
+    setLoading(false);
+  };
+  return (
+    <div className="min-h-screen mt-32">
+      <div className="flex flex-col items-center mb-6">
+        <Lottie animationData={paymentLottie} className="w-40 h-40" />
+        <h2 className="text-2xl font-semibold text-center mt-4">
+          Course Payment
+        </h2>
+      </div>
+
+      <form
+        onSubmit={handleSubmit}
+        className="space-y-4 bg-gray-50 p-6 py-11 px-7 rounded-xl shadow-md w-full max-w-md mx-auto border border-gray-600"
+      >
+        <CardElement className="p-2 border border-gray-600 rounded" />
+        <button
+          className="btn mt-4 bg-gradient-to-r from-[#ef7706] to-[#fa9a1b] hover:from-[#fa9a1b] hover:to-[#ef7706] text-white w-full"
+          type="submit"
+          disabled={!stripe || loading}
+        >
+          {loading ? "Processing..." : "Pay & Become a Member"}
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default PaymentForm;
--- src/pages/CourseDetails/CourseDetails.jsx
+++ src/pages/CourseDetails/CourseDetails.jsx
@@ -27,7 +27,7 @@ const CourseDetails = () => {
   const course = data.find((c) => c._id.toString() === id);
   const userEmail = user?.email || "";
 
-  const userCourseData = {
+  const userCourseData = { 
     email: userEmail,
     courseId: course._id,
     title: course.title,
@@ -99,7 +99,7 @@ const CourseDetails = () => {
       });
     }
 
-    enrollPost(userCourseData, userEmail)
+    enrollPost(userCourseData, userEmail) 
       .then((res) => {
         if (res.insertedId) {
           return enrollIncrement(course._id, totalEnrolled + 1, userEmail);
@@ -229,7 +229,7 @@ const CourseDetails = () => {
                     Enrolled
                   </button>
                 ) : (
-                  <Link to="/payment"><button
+                  <Link to="/payment" state={{courseId: course._id}}><button
                     onClick={handleUserCourses}
                     className={`w-full text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 ${
                       enrollLimitReached || remainingSeat <= 0
